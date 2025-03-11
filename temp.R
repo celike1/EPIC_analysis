0c26f3858bb8db7a7ad5cc441400d164312c9a2c
celike1 <ece01celik@gmail.com>
2025-03-11 10:18:24 -0700

    r files related to life tables

--- temp.R
+++ temp.R
@@ -2,6 +2,7 @@ library(epicUS)
 library(dplyr)
 library(tidyr)
 
+
 settings <- get_default_settings()
 settings$record_mode <- 2
 # settings$agent_stack_size <- 0
@@ -10,7 +11,7 @@ settings$n_base_agents <- 1e6
 
 init_session(settings = settings)
 
-input <- Cget_inputs()
+input <- get_input()
 
 # set time horizon
 time_horizon <- 1
@@ -18,7 +19,7 @@ input$values$global_parameters$time_horizon <- time_horizon
 
 run(input = input$values)
 events <- as.data.frame(Cget_all_events_matrix())
-
+terminate_session()
 
 ## checking if the event has any 7
 unique(events$event)
@@ -28,23 +29,53 @@ table(events$event)
 events <- events %>%
   mutate(age_and_local = floor(local_time + age_at_creation))
 
-# probability of death by age group and sex
-death_prob <- events %>%
+
+age_info<-events %>%
+  mutate(age_and_local = floor(local_time + age_at_creation)) %>%
+  group_by(age_and_local,female) %>%
+  summarise(count = n())
+
+events_filtered<- events %>%
+  mutate(death= ifelse(event==13,1,0)) %>%
+  group_by(id) %>%
+  mutate(ever_death = sum(death)) %>%
+  filter(event==14) %>%
+  ungroup()
+
+
+death_prob<- events_filtered %>%
   group_by(age_and_local, female) %>%
   summarise(
-    #total number of individuals in each group.
     total_count = n(),
-    #number of deaths (event == 14) in each group.
-    death_count = sum(event == 13),
+    death_count = sum(ever_death==1),
     death_probability = death_count / total_count
-  ) %>%
-  ungroup() %>%
-  select(age_and_local, female, death_probability) %>%
-  pivot_wider(names_from = female, values_from = death_probability, names_prefix = "sex_")
+  )
+# %>%
+#   ungroup() %>%
+#   select(age_and_local, female, death_probability) %>%
+#   pivot_wider(names_from = female, values_from = death_probability, names_prefix = "sex_")
 
 colnames(death_prob) <- c("Age", "Male", "Female")
 
-death_prob
+
+
+# # probability of death by age group and sex
+# death_prob <- events %>%
+#   group_by(age_and_local, female) %>%
+#   summarise(
+#     #total number of individuals in each group.
+#     total_count = n(),
+#     #number of deaths (event == 14) in each group.
+#     death_count = sum(event == 13),
+#     death_probability = death_count / total_count
+#   ) %>%
+#   ungroup() %>%
+#   select(age_and_local, female, death_probability) %>%
+#   pivot_wider(names_from = female, values_from = death_probability, names_prefix = "sex_")
+#
+# colnames(death_prob) <- c("Age", "Male", "Female")
+#
+# death_prob
 
 
 USlifetables <-read.csv("USLifeTables.csv", header = FALSE)
